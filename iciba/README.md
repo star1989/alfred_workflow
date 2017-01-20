4176a1e10ed7ac0dce25714dca5619e8c66bac45
star <1029361533@qq.com>
2017-01-20 09:54:04 +0800

    金山翻译workflow

---
+++ iciba/README.md
@@ -0,0 +1,26 @@
+# workflow
+## 金山翻译
+### **功能:**
+* 翻译
+* 音标
+* 发音
+* 复制(当前选中行文本)
+
+### 按键
+*dc*
+> 对应中文的单词,而且也离左手近,比较方便(主要是懒)
+
+### 效果
+![翻译截图]()(http://bimg.cxstars.com/2017-01-20-14848765913701.jpg)
+
+### 金山API
+项目里我已经放上我自己的API KEY,方便跟我一样懒的道友们.
+不过也建议大家自行申请,非常方便.[金山词霸API]()(http://open.iciba.com/?c=api)
+修改文件`jsfy.py`:
+`app_key = "put your api key"`
+
+## 参考资料
+* [Alfred-Workflow Python]()(http://www.deanishe.net/alfred-workflow/)
+* [Alfred office help]()(https://www.alfredapp.com/help/workflows/)
+
+
---
+++ iciba/jsfy.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#
+# 金山翻译
+# author by star
+import sys
+from workflow import Workflow,web
+api_url = "http://dict-co.iciba.com/api/dictionary.php"
+app_key = "DB77106E3D56416869155B591C53C9C5"
+ICON_DEFAULT = 'icon.png'
+update_settings = {'version': '1.0.0', 'github_slug': 'star1989/alfred_workflow'}
+wf = Workflow(update_settings=update_settings)
+word = sys.argv[1]
+
+def getTransResult():
+    url = api_url+"?w="+soword+"&type=json&key="+app_key
+    res = web.get(url)
+    return res.json()
+
+def genAlfred(data):
+    if data.get('word_name','') != "":
+        wf.add_item(
+            title=u"am:[" + data['symbols'][0]['ph_am'] + "] ,en:["+data['symbols'][0]['ph_en']+"]",
+            subtitle=u"phonic for " + word + "[by star]",
+            arg=u"am:[" + data['symbols'][0]['ph_am'] + "] ,en:["+data['symbols'][0]['ph_en']+"]==="+data['symbols'][0]['ph_am_mp3'],
+            # arg=u"am:[" + data['symbols'][0]['ph_am'] + "] ,en:["+data['symbols'][0]['ph_en']+"]",
+            valid=True,
+            icon=ICON_DEFAULT)
+        #mean
+        for part in data['symbols'][0]['parts']:
+            cur_title = u""+part['part']
+            cur_title = cur_title + ','.join(part['means'])
+            sub_title = u""+part['part']+" for "  + word + "[by star]"
+            wf.add_item(
+                title=cur_title,
+                subtitle=sub_title,
+                arg=cur_title+"==="+data['symbols'][0]['ph_am_mp3'],
+                valid=True,
+                icon=ICON_DEFAULT)
+    else:
+        wf.add_item(title=u'error...',
+                    subtitle=u"something has wrong...",
+                    arg=data['word_name'],
+                    valid=True,
+                    icon=ICON_DEFAULT)
+    wf.send_feedback()
+
+def main(wf):
+    data = getTransResult()
+    genAlfred(data)
+    exit()
+
+if __name__ == u"__main__":
+    update_settings = {'version': '1.0.0','github_slug':'0.0.0'}
+    wf = Workflow(update_settings=update_settings)
+    sys.exit(wf.run(main))
