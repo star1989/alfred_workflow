9ce44df8791a7c8ce3a93a71b39d9970ee3b67fb
star1989 <15658101580@163.com>
2017-01-21 21:04:35 +0800

    v1.1: 1.添加中文查询英文结果 2.兼容没有音标&读音的单词

--- iciba/jsfy.py
+++ iciba/jsfy.py
@@ -5,6 +5,8 @@
 # author by star
 import sys
 from workflow import Workflow,web
+import urllib
+
 api_url = "http://dict-co.iciba.com/api/dictionary.php"
 app_key = "DB77106E3D56416869155B591C53C9C5"
 ICON_DEFAULT = 'icon.png'
@@ -13,29 +15,51 @@ wf = Workflow(update_settings=update_settings)
 word = sys.argv[1]
 
 def getTransResult():
-    url = api_url+"?w="+word+"&type=json&key="+app_key
+    url = api_url+"?w="+urllib.quote(word)+"&type=json&key="+app_key
     res = web.get(url)
     return res.json()
 
 def genAlfred(data):
-    if data.get('word_name','') != "":
-        wf.add_item(
-            title=u"am:[" + data['symbols'][0]['ph_am'] + "] ,en:["+data['symbols'][0]['ph_en']+"]",
-            subtitle=u"phonic for " + word + "[by star]",
-            arg=u"am:[" + data['symbols'][0]['ph_am'] + "] ,en:["+data['symbols'][0]['ph_en']+"]==="+data['symbols'][0]['ph_am_mp3'],
-            valid=True,
-            icon=ICON_DEFAULT)
-        #mean
-        for part in data['symbols'][0]['parts']:
-            cur_title = u""+part['part']
-            cur_title = cur_title + ','.join(part['means'])
-            sub_title = u""+part['part']+" for "  + word + "[by star]"
+    if data.get('word_name') != None:
+        if(data['symbols'][0]['parts'][0].get("part_name") != None):
+            mp3 = ""
+            #mean zh
+            if data['symbols'][0]['symbol_mp3'] != "":
+                mp3 = data['symbols'][0]['symbol_mp3']
             wf.add_item(
-                title=cur_title,
-                subtitle=sub_title,
-                arg=cur_title+"==="+data['symbols'][0]['ph_am_mp3'],
+                title=u"拼音:[" + data['symbols'][0]['word_symbol'] + "]",
+                subtitle=u"phonic for " + word.decode('utf-8') + "[by star]",
+                arg=u"拼音:[" + data['symbols'][0]['word_symbol'] + "]==="+mp3,
                 valid=True,
                 icon=ICON_DEFAULT)
+            for part in data['symbols'][0]['parts'][0]['means']:
+                cur_title = u""+part['word_mean']
+                sub_title = u"zh for "  + word.decode('utf-8') + "[by star]"
+                wf.add_item(
+                    title=cur_title,
+                    subtitle=sub_title,
+                    arg=cur_title+"==="+data['symbols'][0]['symbol_mp3'],
+                    valid=True,
+                    icon=ICON_DEFAULT)
+        else:
+            # mean en
+            if data['symbols'][0]['ph_am'] != "":
+                wf.add_item(
+                    title=u"am:[" + data['symbols'][0]['ph_am'] + "] ,en:[" + data['symbols'][0]['ph_en'] + "]",
+                    subtitle=u"phonic for " + word + "[by star]",
+                    arg=u"am:[" + data['symbols'][0]['ph_am'] + "] ,en:[" + data['symbols'][0]['ph_en'] + "]===" + data['symbols'][0]['ph_am_mp3'],
+                    valid=True,
+                    icon=ICON_DEFAULT)
+            for part in data['symbols'][0]['parts']:
+                cur_title = u"" + part['part']
+                cur_title = cur_title + ','.join(part['means'])
+                sub_title = u"" + part['part'] + " for " + word + "[by star]"
+                wf.add_item(
+                    title=cur_title,
+                    subtitle=sub_title,
+                    arg=cur_title + "===" + data['symbols'][0]['ph_am_mp3'],
+                    valid=True,
+                    icon=ICON_DEFAULT)
     else:
         wf.add_item(title=u'error...',
                     subtitle=u"something has wrong...",
